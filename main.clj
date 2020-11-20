928fd08024550ed24dd47e459b865cdd56416fad
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-20 14:38:04 -0500

    Create main.clj

---
+++ main.clj
@@ -0,0 +1,17 @@
+(defn staircase
+  []
+  (agent {}))
+
+(defn base!
+  [sc base-key base-val]
+  (send sc
+    #(assoc % base-key basw-val)))
+
+(defn step!
+  [sc result needed-keys func]
+  (let [
+        uses (map @sc needed-keys)]
+    (when (every? identity uses)
+      (send sc
+        #(assoc % result
+          (apply func uses))))))
