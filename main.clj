5d10a5290240a92e60052deee8777f09d602db1a
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-25 13:32:33 -0500

    upd

--- main.clj
+++ main.clj
@@ -1,20 +1,16 @@
-(defmacro fn->
-  [& args]
-  `(fn [x#] (-> x# ~@args)))
-
-(defmacro when-not->
+defmacro when-not->
   [basis pred & args]
   `(when-not (~pred ~basis) (-> ~basis ~@args)))
 
 (defn snowball
   [start]
-  (agent (with-meta start {
-                            :snowball true
-                            :sent-keys #{}})))
+  (agent start :meta {
+                       :snowball true
+                       :sent-keys #{}}))
 
 (defn key-sent?
   [target-key]
-  (comp target-key :sent-keys meta deref))
+  (comp target-key :sent-keys meta))
 
 (defn step!
   [sb result needed-keys func]
@@ -22,14 +18,14 @@
         uses (map @sb needed-keys)]
     (when (every? identity uses)
       (when-not-> sb (key-sent? result)
-        (send (fn->
-          (vary-meta update :sent-keys
-            #(conj % result))
-          (assoc result (apply func uses))))))))
+        (send assoc result (apply func uses))
+        (alter-meta! update :sent-keys
+          #(conj % result))))))
 
 (defmacro try!
   [sb paths*]
-  `(do ~@(map (fn [[x _ f _ needed]] `(step! ~sb ~x ~needed ~f))
+  `(do ~@(map (fn [[x _ f _ needed]]
+    `(step! ~sb ~x ~needed ~f))
     (partition 5 paths*))))
 
 (defn returning
